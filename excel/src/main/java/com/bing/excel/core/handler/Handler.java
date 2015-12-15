3673e6548d9c973ddef91554920d08aabbdbd548
bingyulei007 <bingyulei008@gmail.com>
2015-12-15 14:01:51 +0800

    加入接口

---
+++ excel/src/main/java/com/bing/excel/core/handler/Handler.java
@@ -0,0 +1,26 @@
+package com.bing.excel.core.handler;
+/**  
+ * 创建时间：2015-12-14下午4:44:52  
+ * 项目名称：excel  
+ * @author shizhongtao  
+ * @version 1.0   
+ * @since JDK 1.7
+ * 文件名称：Handler.java  
+ * 类说明：  转换excel数据到类实体时候  的抽象对象
+ */
+public interface Handler<T> {
+
+	/**
+	 * <p>Title: 处理转换的逻辑</p>
+	 * <p>Description: </p>
+	 */
+	void process();
+	
+	
+	/**
+	 * <p>Title: 得到处理结果</p>
+	 * <p>Description: </p>
+	 * @return
+	 */
+	T getResult();
+}
